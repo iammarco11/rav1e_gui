0fbf2df8f4eb73b73060b91028173c47eb466d4b
Akshay Praveen Nair <akshaypraveennair11@gmail.com>
2020-04-03 23:25:00 +0530

    Initial commit: WIP: Initialized the Qt widget

---
+++ gui.cpp
@@ -0,0 +1,15 @@
+#include "gui.h"
+#include "ui_gui.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
